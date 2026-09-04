222614b092967203d76194c5b1530626904f64f2
Mike Usidamen <m.usidamen@gmail.com>
2026-09-04 21:58:53 +0100

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Home from './pages/Home';
@@ -16,9 +17,20 @@ import VoterEducation from './pages/VoterEducation';
 import Entrepreneurship from './pages/Entrepreneurship';
 import Contact from './pages/Contact';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="flex flex-col min-h-screen bg-[var(--background)]">
         <Navbar />
         <main className="flex-grow pt-20">
